c0198e1221689cc9846ba2ab97d368d9f3eb8d6e
Baptiste Pauchet <baptistep@pc-fixe-baptiste.home>
2021-11-28 22:33:20 +0100

    Mise a jour

--- script-arduino/script-arduino.ino
+++ script-arduino/script-arduino.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+int vall11;
+float val2;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Emartee.com");
+  Serial.println ("tension:") ;
+  Serial.print("V") ;
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+
+{
+
+  float temp ;
+
+  vall11=analogRead(1) ;
+
+  temp = vall11/4.092 ;
+
+  val2=(temp/10) ;
+
+  Serial.println(val2) ;
+
+  delay(1000) ;
 
 }
+  
--- script-python/script.py
+++ script-python/script.py
@@ -2,4 +2,9 @@ from tkinter import*
 
 screen=Tk()
 
+
+screen.minsize("750","550")
+screen.config(bg="black")
+screen.title("Telemetrie BWW M3 ")
+
 screen.mainloop()
